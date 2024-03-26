563335cd6337344cab845ef459e7277f6a32db39
Perttunen Jari (LUKE) <jari.perttunen@luke.fi>
2024-03-26 14:43:31 +0200

    Climate data and climate data configuration.

--- climateconfig.py
+++ climateconfig.py
@@ -1,5 +1,4 @@
-climate_scenarios =["data/tranCanESM2.rcp45.rda","data/tranCanESM2.rcp45.rda",
-                    "data/tranCanESM2.rcp85.rda","data/tranCNRM.rcp45.rda",
-                    "data/tranCNRM.rcp85.rda"]
+climate_scenarios =["data/tranCanESM2.rcp45.rda","data/tranCanESM2.rcp85.rda",
+                    "data/tranCNRM.rcp45.rda","data/tranCNRM.rcp85.rda"]
 scenarioid=1
 climateid=1
