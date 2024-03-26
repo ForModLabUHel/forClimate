d41b95a2f8e34ec0c352ee2654446c100f4dbad2
Perttunen Jari (LUKE) <jari.perttunen@luke.fi>
2024-03-26 14:44:16 +0200

    Selecting current climate (random) and climate weather (deterministic).

--- sampling.py
+++ sampling.py
@@ -1,4 +1,5 @@
 import numpy as np
+import rpy2.robjects
 
 def current_climate_selector(nyears:int,simyears:int):
     """
@@ -13,13 +14,13 @@ def current_climate_selector(nyears:int,simyears:int):
     #Sample nyears years out of simyears
     years = np.random.randint(1,nyears,simyears)
     #Now: generate 365 repetitions for each year
-    repeat_years = np.repeat(years-1,days)
+    repeat_years = np.repeat((years-1)*days,days)
     #Split to simyears vectors (vector of vectors)
     split_years = np.split(repeat_years,simyears)
     #Add day number to each year
     add_days = split_years[:]+np.array(range(1,days+1))
     #Join all vectors into one single vector indexing daily weather
-    ravel_days = np.ravel(add_days)
+    ravel_days = rpy2.robjects.vectors.IntVector(np.ravel(add_days))
     return ravel_days
 
 def climate_scenario_selector(csdb_start:int,sim_start:int,simyears):
@@ -28,7 +29,7 @@ def climate_scenario_selector(csdb_start:int,sim_start:int,simyears):
     @param csdb_start Start calendar year for the data in climate scenario data
     @param sim_start calendar year for the simulation start
     @param simyears Simulation years
-    @retval ravel_days Vector of day numbers to index daily weather data for simulation years
+    @retval ravel_days Vector of day numbers to index daily climate scenario data for simulation years
     @note Selection is determinstic for consecutive `simyears` years.
     """
     days=365
@@ -37,5 +38,5 @@ def climate_scenario_selector(csdb_start:int,sim_start:int,simyears):
     repeat_years = np.repeat((years-1)*days,days)
     split_years = np.split(repeat_years,simyears)
     add_days = split_years[:]+np.array(range(1,days+1))
-    ravel_days = np.ravel(add_days)
+    ravel_days = rpy2.robjects.vectors.IntVector(np.ravel(add_days))
     return ravel_days
