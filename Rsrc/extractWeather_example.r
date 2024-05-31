d4f30dafa2a31174c0e0f0c2340203bdc6b7b5e9
ForModLabUHel <francesco.minunno@helsinki.fi>
2024-05-31 14:39:56 +0100

    update

--- Rsrc/extractWeather_example.r
+++ Rsrc/extractWeather_example.r
@@ -1,29 +1,41 @@
 library(data.table)
-currClimIDs <- fread("C:/Users/minunno/Documents/research/weather/grid_coords")
+currClimIDs <- fread("C:/Users/checc/Documents/research/weather/grid_coords")
 setnames(currClimIDs,c("long_deg","lat_deg"),c("x","y"))
 currclimIDs_tran <- c(200,500,700, 1385,2305,2805,3422)
 exampleCoords <- as.matrix(currClimIDs[currclimIDs_tran,.(x,y)])
 exampleCoords[3,] <- c(27.1,64.44)
 
-load("C:/Users/minunno/Documents/research/weather/CurrClim.rdata")
+load("C:/Users/checc/Documents/research/weather/CurrClim.rdata")
 
 coords <- exampleCoords
 startYear <- 1981
-outDataBase <- TRUE
+DataBaseFormat <- TRUE
 
-climateIn <- extractWeatherPrebas(exampleCoords,1991,TRUE)$dataBase
+currClimData <- extractWeatherPrebas(exampleCoords,startYear,
+                                     DataBaseFormat,currClim_dataBase)$dataBase
 
-oo <- sampleTypicalYears(climateIn)
+startYear2 <- 2022
+DataBaseFormat <- FALSE
+climateChangeData <- extractWeatherPrebas(exampleCoords,startYear2,
+                                          DataBaseFormat,climateChange_dataBase)
+
+typicalSample <- sampleTypicalYears(currClimData)
+
+dim(oo$PAR)
 
 
 
-currClimIDs
-RCPsIDs
-coords <- exampleCoords
+1. load the databases
+2. process the data (extract the sites using the coordinates and the years used in the simulations)
+point two needs to be done for climate change scenario and the Current Climate
+2.1 currentClimate database (line 10 to 14)
+2.2 climate change data extraction (lines 17 to 20)
+2.3 update the climateIds in siteInfo
+siteInfo[,2] <- climateChangeData$climIDs
 
-xx <- coords[1,]
-x <- xx[1] - currClimIDs$long_deg
-y <- xx[2] - currClimIDs$lat_deg
-hip = sqrt(x^2 + y^2)
-which.min(hip)
-hip[200]
+3. run simulations:
+  3.1 sample 5 typical years: typicalSample <- sampleTypicalYears(currClimData)
+3.2 extract the correct years from the climateChangeData database !!!!!!every year has 365 days
+
+
+
